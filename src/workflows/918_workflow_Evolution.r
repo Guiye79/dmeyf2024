83d9c89ed3e2d3401e4104a40bcd8a8bf7dcfcce
Guillermo Piazza <74121789+Guiye79@users.noreply.github.com>
2024-11-03 16:27:59 -0300

    Update 918_workflow_Evolution.r

--- src/workflows/918_workflow_Evolution.r
+++ src/workflows/918_workflow_Evolution.r
@@ -429,26 +429,26 @@ wf_agosto_base_2024_11_01 <- function( pnombrewf )
 
   # Etapas preprocesamiento
   CA_catastrophe_base( metodo="MachineLearning")
-  FEintra_manual_base()
+  # FEintra_manual_base()
   DR_drifting_base(metodo="deflacion")
   FEhist_base()
 
-  FErf_attributes_base( arbolitos= 20,
-    hojas_por_arbol= 16,
-    datos_por_hoja= 1000,
-    mtry_ratio= 0.2
-  )
+  # FErf_attributes_base( arbolitos= 20,
+  #   hojas_por_arbol= 16,
+  #   datos_por_hoja= 1000,
+  #   mtry_ratio= 0.2
+ # )
 
-   CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
+  #  CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
 
   # Etapas modelado
-  ts8 <- TS_strategy_base8()
-  ht <- HT_tuning_base( bo_iteraciones = 40 )  # iteraciones inteligentes
+ # ts8 <- TS_strategy_base8()
+  # ht <- HT_tuning_base( bo_iteraciones = 40 )  # iteraciones inteligentes
 
   # Etapas finales
-  fm <- FM_final_models_lightgbm( c(ht, ts8), ranks=c(1), qsemillas=5 )
-  SC_scoring( c(fm, ts8) )
-  KA_evaluate_kaggle()  # genera archivos para Kaggle
+ #  fm <- FM_final_models_lightgbm( c(ht, ts8), ranks=c(1), qsemillas=5 )
+ #  SC_scoring( c(fm, ts8) )
+ #  KA_evaluate_kaggle()  # genera archivos para Kaggle
 
   return( exp_wf_end() ) # linea workflow final fija
 }
